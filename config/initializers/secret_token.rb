b1458f587283113cbf72c1b3c03771e05f02c2d8
mather <mather314@gmail.com>
2011-04-19 00:57:31 +0900

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CutDownCommunicator::Application.config.secret_token = 'a5e9486638c70a4dd5d847bfe7e9e8763200df93f7a5bc95752ee39f2f558bf35a37ecfa592577a37b28a6607bc737c53e4b1119f6d6b72a65e352433d8c1d34'
